641cfc4169e6ccaaa34a196ef4161ec6dbcf8839
StepanMozhgaev <1143573334ggg@gmail.com>
2022-12-21 17:34:25 +0400

    подправил код

--- Lab2/Iter.py
+++ Lab2/Iter.py
@@ -1,21 +1,18 @@
 from next_element import next_element
 
 
-class Iterator:
+class Iter:
     def __init__(self, path: str):
-        """
-        :param path: path to file
-        """
         self.path = path
+        """
+        путь к файлу
+        """
+
 
     def __next__(self) -> str:
-        """
-        :return: next element
-        """
         self.path = next_element(self.path)
         return self.path
+    """
+    возвращает следующий элемент
+    """
 
-
-if __name__ == "__main__":
-    Iterator_1 = Iterator("C:\\Users\\0\\python_var_7\\dataset\\zebra\\0001.jpg")
-    print(Iterator_1.__next__())
