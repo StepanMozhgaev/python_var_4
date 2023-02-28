0b48deef8f02bccfbae147b28e8ffc86f2c099a5
StepanMozhgaev <1143573334ggg@gmail.com>
2023-02-28 08:11:34 +0400

    поправил замечания

--- Lab2/Data.py
+++ Lab2/Data.py
@@ -1,24 +1,22 @@
-import os
 import csv
+import os
 
-
-default = ["zebra", "bay horse"] # дефолтные классы
+DEFAULT = ["zebra", "bay horse"]  # дефолтные классы
 
 
 class Data:
     def __init__(self, directory: str) -> None:
+        """
+        :param directory: dir for annotation file
+        """
         self.directory = directory
         self.str_num = 0
-    """
-    self.str_num - кол-во строк в аннотации
-    """
 
     def adder(self, path: str, class_name: str, img_name: str) -> None:
         """
-        path - путь к директории
-        class_name - имя дефолтного класса
-        img_name - имя изображения
-        функция записывает в файл-аннотацию информацию об изображении
+        :param path: new dir path
+        :param class_name: class name
+        :param img_name: image name
         """
         with open(os.path.join(path, self.directory, "annotation.csv"), "a", encoding="utf-8", newline="") as file:
             writer = csv.writer(file, quoting=csv.QUOTE_ALL)
@@ -28,9 +26,6 @@ class Data:
                     "Relative path",
                     "Class"
                 ])
-                """
-                если кол-во строк равно 0, то заголовок
-                """
                 self.str_num += 1
             writer.writerow([os.path.join(path, self.directory, class_name, img_name),
                              os.path.join(self.directory, class_name, img_name), class_name])
