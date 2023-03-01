f5a534f201dd8b64469cef9e7c616abd008024bd
StepanMozhgaev <118831745+StepanMozhgaev@users.noreply.github.com>
2023-03-01 17:32:44 +0400

    Update rand.py

--- Lab3/rand.py
+++ Lab3/rand.py
@@ -5,14 +5,14 @@ import tqdm
 import random
 
 
-def copy_dataset(directory_obj: str, c_directory_obj: str, name: str):
+def copy_dataset(directory_obj: str, c_directory_obj: str, name: str) -> None:
     """Copies all files from one folder to another, return NONE.
     Args:
         directory_obj (str): the path of the source folder.
         c_directory_obj (str): folder path to copy.
         name (str): object class.
     """
-    c_directory_obj1 = f"{c_directory_obj}\\dataset_3"
+    c_directory_obj1 = os.path.join(c_directory_obj, "dataset_3")
     if not os.path.isdir(c_directory_obj1):
         os.makedirs(c_directory_obj1)
 
@@ -33,31 +33,30 @@ def copy_dataset(directory_obj: str, c_directory_obj: str, name: str):
     for i in tqdm.tqdm(data):
         so = str(r_list[j])
         copy_list.append(so)
-        shutil.copy(directory_obj + "\\" + i, c_directory_obj1 + "\\" + so + '.jpeg')
+        shutil.copy(os.path.join(directory_obj, i), os.path.join(c_directory_obj1, f"{so}.jpeg"))
         j += 1
 
     write_csv_copy(c_directory_obj1, name, copy_list)
 
 
-def write_csv_copy(c_directory_obj: str, name: str, copy_list: list):
+def write_csv_copy(c_directory_obj: str, name: str, copy_list: list) -> None:
     """Writes the absolute and relative path of the image to csv, return NONE.
     Args:
         c_directory_obj (str): folder path to copy.
         name (str): object class.
         copy_list (list): numbers of copied objects.
     """
-    file = f"{c_directory_obj}\\rand.csv"
-    f = open(file, "a", encoding="utf-8", newline="")
-    f_writer = csv.DictWriter(f, fieldnames=["Absolut_path", "Relative_patch", "Class"], delimiter="|")
+    file = os.path.join(c_directory_obj, "rand.csv")
+    with open(file, "a", encoding="utf-8", newline="") as f:
+        f_writer = csv.DictWriter(f, fieldnames=["Absolut_path", "Relative_patch", "Class"], delimiter="|")
+        r_directory_obj = "dataset_3"
+        for i in copy_list:
+            f_writer.writerow(
+                {"Absolut_path": os.path.join(c_directory_obj, i),
+                 "Relative_patch": os.path.join(r_directory_obj, i), "Class": name})
 
-    r_directory_obj = "dataset_3"
 
-    for i in copy_list:
-        f_writer.writerow(
-            {"Absolut_path": c_directory_obj + "\\" + i, "Relative_patch": r_directory_obj + "\\" + i, "Class": name})
-
-
-def main():
+def main() -> None:
     c_directory = "C:\\Users\\0\\python_var_7\\dataset\\new_dataset_2"
     directory_zebra = "C:\\Users\\0\\python_var_7\\dataset\\zebra"
     directory_bay_horse = "C:\\Users\\0\\python_var_7\\dataset\\bay_horse"
